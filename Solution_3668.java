94c83562e4a8841d3bb8024495c4f2cd8c361378
divya boolchandani <divyaboolchandani2104@gmail.com>
2026-02-27 11:58:05 +0530

    leetcode 3668 recover finishing  order solution

---
+++ Solution_3668.java
@@ -0,0 +1,15 @@
+class Solution_3668{
+    public int[] recoverOrder(int[] order, int[] friends) {
+    int[] result = new int[friends.length];
+    int k = 0;
+    for (int i = 0; i < order.length; i++) {
+        for (int j = 0; j < friends.length; j++) {
+            if (order[i] == friends[j]) {
+                result[k++] = order[i];
+                break;
+            }
+        }
+    }
+    return result;
+}
+    }

01d2d181ae86f27c59b8e64440a1319468e25c4c
divya boolchandani <divyaboolchandani2104@gmail.com>
2026-05-13 00:32:32 +0530

    square of a sorted array

---
+++ Leetcode_977.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int result[]=new int[nums.length];
+        int i=0;
+        int j=nums.length-1;
+        int k=result.length-1;
+        while(i<=j){
+            if(nums[i]*nums[i]>=nums[j]*nums[j]){
+                result[k--]=nums[i]*nums[i];
+                i++;
+            }else{
+                result[k--]=nums[j]*nums[j];
+                j--;
+            }
+        }
+        return result;
+    }
+}

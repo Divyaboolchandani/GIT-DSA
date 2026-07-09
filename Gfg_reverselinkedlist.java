8667ada5a2c8dd40a7ca1fcd53320e89b939c316
divya boolchandani <divyaboolchandani2104@gmail.com>
2026-07-09 23:40:53 +0530

    reverse a linked list code

---
+++ Gfg_reverselinkedlist.java
@@ -0,0 +1,30 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        if(head==null||head.next==null){
+            return head;
+        }
+        Node prev=null;
+            Node curr=head;
+            Node next;
+            while(curr!=null){
+                next=curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
+        }
+        return prev;
+    }
+}

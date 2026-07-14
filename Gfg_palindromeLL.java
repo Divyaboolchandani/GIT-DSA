9203df99a881fe4f88ab5328a68075ea0c945fe0
divya boolchandani <divyaboolchandani2104@gmail.com>
2026-07-15 02:12:06 +0530

    gfg- palindrome  linked list -java solution

---
+++ Gfg_palindromeLL.java
@@ -0,0 +1,51 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public Node midnode(Node head){
+            Node slow=head;
+            Node fast=head;
+            while(fast!=null&&fast.next!=null){
+                slow=slow.next;
+                fast=fast.next.next;
+            }
+            return slow;
+    }
+    public boolean isPalindrome(Node head) {
+        // code here
+        
+         if(head==null||head.next==null){
+             return true;
+         }
+         Node midnode=midnode(head);
+         
+         Node prev=null;
+         Node curr=midnode;
+         Node next;
+         while(curr!=null){
+             next=curr.next;
+             curr.next=prev;
+             prev=curr;
+             curr=next;
+         }
+         
+         Node left=head;
+         Node right=prev;
+         while(right!=null){
+             if(left.data!=right.data){
+                 return false;
+             }
+             left=left.next;
+             right=right.next;
+         }
+         return true;
+    }
+}
